1a27b68a1af08d847247a2e12c0876cf633829f9
fengzilong <feng_zilong@163.com>
2018-04-25 22:16:09 +0800

    fix: remove querystring in location.hash

--- src/hash/getHash.js
+++ src/hash/getHash.js
@@ -1,7 +1,7 @@
 function getHash( url = location.href ) {
 	const index = url.indexOf( '#' )
 	if ( ~index ) {
-		return url.slice( index + 1 )
+		return url.slice( index + 1 ).split( '?' )[ 0 ]
 	} else {
 		return ''
 	}
