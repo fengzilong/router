9df12f807a81107634968cb499e045656b2a5e58
fengzilong <feng_zilong@163.com>
2019-03-26 13:22:40 +0800

    feat: decodeURIComponent for hash

--- src/hash/getHash.js
+++ src/hash/getHash.js
@@ -1,8 +1,11 @@
 function getHash( url = location.href ) {
   const index = url.indexOf( '#' )
+
   if ( ~index ) {
-    return url.slice( index + 1 ).split( '?' )[ 0 ]
+    const hash = url.slice( index + 1 ).split( '?' )[ 0 ]
+    return hash ? decodeURIComponent( hash ) : ''
   }
+
   return ''
 }
 
